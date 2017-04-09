79c33577eeb63b35b1079d1c3caf4b047a64c7b0
33haji <one_for_all_0828@yahoo.co.jp>
2017-04-09 21:58:52 +0900

    基本的なhomeページを表示できるよう設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  get 'admin_login/' => 'admin_login#index'
+  post 'admin_login/login' => 'admin_login#login'
+  post 'admin_login/logout' => 'admin_login#logout'
+
+  namespace :manage do
+    get :table_list
+    get :column_list
+  end
 end
