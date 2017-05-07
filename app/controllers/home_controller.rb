13fa867b4c873064d331dff58eff55f8e50646ec
33haji <one_for_all_0828@yahoo.co.jp>
2017-05-07 19:25:45 +0900

    トップにランキングを追加等

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,26 @@
 class HomeController < ApplicationController
   def index
-    @shops = Shop.where(delete_flag: false).where.not(latitude: nil, longitude:nil)
+    # 対象の店舗を取得する
+    if params[:area].blank?
+      @shops = Shop.where(delete_flag: false).where.not(latitude: nil, longitude:nil).order(rate: :desc).take(10)
+    else
+      @shops = Shop.where(delete_flag: false, area: params[:area]).where.not(latitude: nil, longitude:nil).order(rate: :desc).take(10)
+    end
+    @shops_in_area = count_shop_in_area
+    # 順位を決定する
+    @ranking = get_ranking(@shops)
+
+    # Googleマップの設定
     @map = Gmaps4rails.build_markers(@shops) do |shop, marker|
       marker.lat shop.latitude
       marker.lng shop.longitude
       marker.infowindow render_to_string(partial: "home/infowindow", locals: { shop: shop })
       marker.json({title: shop.name})
     end
-    @shops_in_area = count_shop_in_area
   end
 
   private
+    # 対象地域のショップ数を算出する
     def count_shop_in_area
       shops_in_area = {}
       areas = Shop.select(:area).uniq
@@ -20,5 +30,27 @@ class HomeController < ApplicationController
       end
       shops_in_area.sort {|(k1, v1), (k2, v2)| v2 <=> v1 }
       shops_in_area
+    end
+
+    # ショップの評価のランキングを作成する
+    def get_ranking(shops)
+      ranking = []
+      count = 1
+      rank = 1
+      pre_rate = 0
+      shops.each do |shop|
+        if ranking.empty? || pre_rate == shop[:rate]
+          ranking << rank
+          pre_rate = shop[:rate]
+        elsif pre_rate != shop[:rate] && count <= 3
+          rank = rank + 1
+          ranking << rank
+          pre_rate = shop[:rate]
+        else
+          ranking << 0
+        end
+        count = count + 1
+      end
+      ranking
     end
 end
